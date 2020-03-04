26d664d6ad2fdaf1dd504252021d29e1133cffb8
Julien Pilla <vrlitwhai.kridanik@gmail.com>
2020-03-04 15:41:47 +0100

    fix some footer issue & reduce font size in contact page

--- src/components/pages/ContactPage.vue
+++ src/components/pages/ContactPage.vue
@@ -1,41 +1,34 @@
 <template>
   <v-app>
     <main-toolbar>
-
     </main-toolbar>
 
     <v-content>
-
       <section class="head--section" >
         <v-parallax :src="require('@/assets/hero.jpeg')" height="250">
-
           <v-layout
             column
             align-center
             justify-center
             class="black--text"
           >
-
-
-
           </v-layout>
-
         </v-parallax>
       </section>
 
       <section>
       <v-container fluid>
-
           <v-layout justify-center>
-            <v-flex xs8>
-              <section>
-                <h1 class="font-weight-bold title mt-5 mb-3"><span class="display-3">Nous contacter</span></h1>
-                <article class="display-1">
-                  <p>Ce site est en cours d'évolution. </p>
-                    <p>Si vous souhaitez faire des remarques ou désirez des informations complémentaires
-                    à son sujet, vous pouvez contacter l'équipe en
-                    charge du projet à l'adresse :</p>
-                  <a href="mailto:dicotopo@cths.fr?">dicotopo@cths.fr</a>
+            <v-flex xs4>
+              <section class="mt-5">
+                <h1 class="title mt-3 mb-3">Nous contacter</h1>
+                <article class="body-1">
+                  <p>
+                     Ce site est en cours d'évolution.
+                     Si vous souhaitez faire des remarques ou désirez des informations complémentaires
+                     à son sujet, vous pouvez contacter l'équipe en
+                     charge du projet à l'adresse suivante : <a href="mailto:dicotopo@cths.fr?">dicotopo@cths.fr</a>
+                  </p>
                 </article>
               </section>
             </v-flex>
@@ -43,22 +36,20 @@
         </v-container>
        </section>
 
-      <section class="foot--section mt-5">
-        <custom-footer></custom-footer>
-      </section>
+      <custom-footer></custom-footer>
 
     </v-content>
   </v-app>
 </template>
 
 <script>
-    import MainToolbar from '../ui/MainToolbar'
-  import CustomFooter from '../ui/CustomFooter'
+import MainToolbar from '../ui/MainToolbar'
+import CustomFooter from '../ui/CustomFooter'
 
-    export default {
-        name: "ContactPage",
-        components: {MainToolbar, CustomFooter}
-    }
+export default {
+  name: 'ContactPage',
+  components: { MainToolbar, CustomFooter }
+}
 </script>
 
 <style scoped>

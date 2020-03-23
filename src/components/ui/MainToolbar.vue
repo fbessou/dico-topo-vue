294b9a5c46ff4f035795b7f683b7ce6961225bbf
Julien Pilla <vrlitwhai.kridanik@gmail.com>
2020-03-23 14:05:50 +0100

    fix toolbar paddings

--- src/components/ui/MainToolbar.vue
+++ src/components/ui/MainToolbar.vue
@@ -1,10 +1,15 @@
 <template>
   <v-app-bar app>
-
-    <v-toolbar-title class="headline text-uppercase" @click="$router.push({name: 'landing'})"
-                     style="cursor: pointer; ">
-
-      <img src="@/assets/logo-dicotopo-cut.png" height="55px" style="padding-top: 8px; padding-left: 40px"/>
+    <v-toolbar-title
+      class="headline text-uppercase"
+      @click="$router.push({ name: 'landing' })"
+      style="cursor: pointer; "
+    >
+      <img
+        src="@/assets/logo-dicotopo-cut.png"
+        height="55px"
+        style="padding-top: 8px; padding-left: 40px"
+      />
     </v-toolbar-title>
 
     <v-spacer></v-spacer>
@@ -17,66 +22,72 @@
       :autofocus="true"
       :loading="mapMarkersAreLoading"
       clearable
-      style="width: 0"
+      style="width: 0; padding-top: 16px"
       color="rgb(211, 47, 47)"
       @keyup.enter.native="onSearchEventPressEnter"
     >
     </v-text-field>
 
-    <v-chip v-show="!!inputTerm && inputTerm.length > 3" class="subheading grey--text text--darken-2 ml-3 mr-2">
+    <v-chip
+      v-show="!!inputTerm && inputTerm.length > 3"
+      class="subheading grey--text text--darken-2 ml-3 mr-2"
+    >
       {{ meta.totalCount }}
     </v-chip>
 
     <span class="toolbar-buttons">
-
-        <span v-if="showGroupBy">
-          <v-layout row>
-            <v-flex>
-              <v-tooltip bottom>
-                <template v-slot:activator="{ on }">
-                  <span v-on="on">Lieux</span>
-                </template>
-                <span>Les résultats de la recherche sont groupés par lieu identifié</span>
-              </v-tooltip>
-            </v-flex>
-            <v-flex>
-               <v-switch
-                 class="ml-2"
-                 style="color: #d32f2f !important"
-                 v-model="groupByOption"
-                 color="lightgrey"
-                 :disabled="mapMarkersAreLoading">
-               </v-switch>
-            </v-flex>
-            <v-flex>
-               <v-tooltip bottom>
-                <template v-slot:activator="{ on }">
-                  <span v-on="on">Toponymes</span>
-                </template>
-                <span>Les résultats de la recherche ne sont pas groupés par lieu identifié mais par forme toponymique</span>
-                </v-tooltip>
-            </v-flex>
-          </v-layout>
-        </span>
-
+      <span v-if="showGroupBy">
+        <v-layout row>
+          <v-flex>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on }">
+                <span v-on="on">Lieux</span>
+              </template>
+              <span
+                >Les résultats de la recherche sont groupés par lieu
+                identifié</span
+              >
+            </v-tooltip>
+          </v-flex>
+          <v-flex>
+            <v-switch
+              class="ml-2"
+              style="color: #d32f2f !important"
+              v-model="groupByOption"
+              color="lightgrey"
+              :disabled="mapMarkersAreLoading"
+            >
+            </v-switch>
+          </v-flex>
+          <v-flex>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on }">
+                <span v-on="on">Toponymes</span>
+              </template>
+              <span
+                >Les résultats de la recherche ne sont pas groupés par lieu
+                identifié mais par forme toponymique</span
+              >
+            </v-tooltip>
+          </v-flex>
+        </v-layout>
       </span>
+    </span>
 
     <v-spacer></v-spacer>
 
-       <v-btn text to="/contact">
-      <span class="mr-2">Contact</span>
+    <v-btn text to="/contact" class="mr-2">
+      <span class="pr-2 pl-2">Contact</span>
     </v-btn>
 
     <v-btn text to="/documentation">
-      <span class="mr-2">API</span>
+      <span class="pr-2 pl-2">API</span>
       <v-icon>code</v-icon>
     </v-btn>
   </v-app-bar>
-
 </template>
 
 <script>
-
 import _ from 'lodash'
 import { mapState, mapGetters, mapActions } from 'vuex'
 
@@ -86,9 +97,7 @@ export default {
     showGroupBy: { type: Boolean, default: false },
     showTimeRange: { type: Boolean, default: false }
   },
-  components: {
-
-  },
+  components: {},
   data () {
     return {
       maxMarkerPerBatch: 5000,
@@ -129,23 +138,35 @@ export default {
         rangeParam: this.computedRangeParam,
         nextLink: nextLink,
         pageSize: this.maxMarkerPerBatch
-      }).then(next => {
-        if (next) {
-          this.searchNextBatchOfMapMarkers(next)
-        }
-      }).catch(r => {
-        console.warn(r)
-      }).finally(r => {
-        this.setMarkersLoading(false)
       })
+        .then(next => {
+          if (next) {
+            this.searchNextBatchOfMapMarkers(next)
+          }
+        })
+        .catch(r => {
+          console.warn(r)
+        })
+        .finally(r => {
+          this.setMarkersLoading(false)
+        })
     },
     onSearchEventPressEnter () {
       this.startNewSearch()
     },
-    ...mapActions('mapmarkers', ['searchMapMarker', 'clearMapMarkers', 'setMarkersLoading']),
+    ...mapActions('mapmarkers', [
+      'searchMapMarker',
+      'clearMapMarkers',
+      'setMarkersLoading'
+    ]),
     ...mapActions('places', ['selectPlace', 'unselectPlace']),
     ...mapActions('PlaceCard', ['clearPlaceCard']),
-    ...mapActions('searchParameters', ['setTerm', 'setGroupbyPlace', 'setRange', 'removeRange'])
+    ...mapActions('searchParameters', [
+      'setTerm',
+      'setGroupbyPlace',
+      'setRange',
+      'removeRange'
+    ])
   },
   watch: {
     inputTerm (oldVal, newVal) {
@@ -165,7 +186,10 @@ export default {
       if (this.$props.showTimeRange && this.selectedTimeRange.length === 2) {
         const newRange = {
           key: 'text-date',
-          operators: [`gte:${this.selectedTimeRange[0]}`, `lte:${this.selectedTimeRange[1]}`]
+          operators: [
+            `gte:${this.selectedTimeRange[0]}`,
+            `lte:${this.selectedTimeRange[1]}`
+          ]
         }
         this.setRange(newRange)
       }
@@ -180,17 +204,37 @@ export default {
     },
     range (oldVal, newVal) {
       if (this.$props.showTimeRange) {
-        if (oldVal !== newVal && !!this.inputTerm && this.inputTerm.length > 2) {
+        if (
+          oldVal !== newVal &&
+          !!this.inputTerm &&
+          this.inputTerm.length > 2
+        ) {
           this.startNewSearch()
         }
       }
     }
   },
   computed: {
-    ...mapState('places', { selectedPlace: 'selectedItem', meta: 'meta', knownYears: 'knownYears' }),
-    ...mapState('mapmarkers', { mapMarkersAreLoading: 'isLoading', mapMarkerItems: 'items' }),
-    ...mapState('searchParameters', ['term', 'groupbyPlace', 'minTermLength', 'range']),
-    ...mapGetters('searchParameters', ['query', 'computedFilterParam', 'computedRangeParam']),
+    ...mapState('places', {
+      selectedPlace: 'selectedItem',
+      meta: 'meta',
+      knownYears: 'knownYears'
+    }),
+    ...mapState('mapmarkers', {
+      mapMarkersAreLoading: 'isLoading',
+      mapMarkerItems: 'items'
+    }),
+    ...mapState('searchParameters', [
+      'term',
+      'groupbyPlace',
+      'minTermLength',
+      'range'
+    ]),
+    ...mapGetters('searchParameters', [
+      'query',
+      'computedFilterParam',
+      'computedRangeParam'
+    ]),
     timeFilterData () {
       const lowerBound = this.knownYears[0].year
       const upperBound = this.knownYears[this.knownYears.length - 1].year
@@ -216,46 +260,45 @@ export default {
 </script>
 
 <style >
-  html {
-    background: lightgrey;
-  }
-  body:after {
-    z-index: 100000;
-    content: 'beta';
-    position: fixed;
-    width: 100px;
-    height: 28px;
-    background: #EE8E4A;
-    top: 7px;
-    left: -28px;
-    text-align: center;
-    font-size: 13px;
-    font-family: sans-serif;
-    text-transform: uppercase;
-    font-weight: bold;
-    color: #fff;
-    line-height: 30px;
-    -ms-transform: rotate(-45deg);
-    -webkit-transform: rotate(-45deg);
-    transform: rotate(-45deg);
-  }
+html {
+  background: lightgrey;
+}
+body:after {
+  z-index: 100000;
+  content: "beta";
+  position: fixed;
+  width: 100px;
+  height: 28px;
+  background: #ee8e4a;
+  top: 7px;
+  left: -28px;
+  text-align: center;
+  font-size: 13px;
+  font-family: sans-serif;
+  text-transform: uppercase;
+  font-weight: bold;
+  color: #fff;
+  line-height: 30px;
+  -ms-transform: rotate(-45deg);
+  -webkit-transform: rotate(-45deg);
+  transform: rotate(-45deg);
+}
 
-  .toolbar-buttons > span {
-    display: flex;
-    line-height: 32px;
-    margin-top: 20px;
-    margin-left: 32px;
-    color: grey;
-    font-family: Roboto, sans-serif;
-  }
+.toolbar-buttons > span {
+  display: flex;
+  line-height: 32px;
+  margin-top: 20px;
+  margin-left: 32px;
+  color: grey;
+  font-family: Roboto, sans-serif;
+}
 
-  .toolbar-buttons span {
-    display: inline-block;
-    vertical-align: middle;
-  }
-  .toolbar-buttons .v-slider__thumb {
-    height: 20px;
-    width: 20px;
-  }
-
+.toolbar-buttons span {
+  display: inline-block;
+  vertical-align: middle;
+}
+.toolbar-buttons .v-slider__thumb {
+  height: 20px;
+  width: 20px;
+}
 </style>

676674810a2aff2d81fcb9631119c83edf2a1cf5
Julien Pilla <vrlitwhai.kridanik@gmail.com>
2020-04-22 15:12:14 +0200

    add commune & place links to the breadcrumbs

--- src/components/AdministrativeBreadcrumbs.vue
+++ src/components/AdministrativeBreadcrumbs.vue
@@ -22,19 +22,38 @@ export default {
   computed: {
     ...mapState('commune', ['region', 'arrondissement', 'canton', 'departement', 'commune']),
     ...mapState('PlaceCard', ['placeItem']),
-    data () {
-      return {
-
-      }
-    },
     items () {
       let items = []
-      // if (this.region) { items.push({ text: this.region.label }) }
-      if (this.departement) { items.push({ text: `${this.departement.label} (${this.departement['insee-code']}) ` }) }
+      // departement
+      if (this.departement) {
+        items.push({
+          text: `${this.departement.label} (${this.departement['insee-code']})`
+        })
+      }
+      // canton
+      if (this.canton) {
+        items.push({
+          text: this.canton.label
+        })
+      }
+      // commune de rattachement
+      if (this.commune.data) {
+        let newItem = { text: this.commune.data.attributes['NCCENR'] }
+        if (this.commune.data.attributes['place-id']) {
+          const route = this.$router.resolve({ name: 'place', params: { placeId: this.commune.data.attributes['place-id'] } })
+          newItem['href'] = route.href
+        }
+        items.push(newItem)
+      }
+      // lieu
+      if (this.placeItem) {
+        let newItem = { text: this.placeItem.label }
 
-      if (this.canton) { items.push({ text: this.canton.label }) }
-      if (this.commune.data) { items.push({ text: this.commune.data.attributes['NCCENR'] }) }
-      if (this.placeItem) { items.push({ text: this.placeItem.label }) }
+        const route = this.$router.resolve({ name: 'place', params: { placeId: this.placeItem.id } })
+        newItem['href'] = route.href
+
+        items.push(newItem)
+      }
 
       return items
     }

b5871912a5d016d8474bfeee2261cc9ca8838d22
Julien Pilla <vrlitwhai.kridanik@gmail.com>
2020-11-27 16:53:51 +0100

    rework AboutPage with sections

---
+++ src/components/pages/about/ContentSection.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <section>
+      <h1 class="font-weight-bold title mt-5 mb-3">
+        <span class="display-1">Contenus</span>
+      </h1>
+      <p>Lorem ipsum</p>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ContentSection'
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/pages/about/UserGuideSection.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <section>
+      <h1 class="font-weight-bold title mt-5 mb-3">
+        <span class="display-1">Mode d'emploi</span>
+      </h1>
+      <p>Lorem ipsum</p>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'UserGuideSection'
+}
+</script>
+
+<style scoped>
+</style>
